f9b73fd50d6ec6c8829e437695c9ac854a0fb6e9
Mark Gossa <16326726+markgossa@users.noreply.github.com>
2021-01-25 22:34:29 +0000

    Add initial MessageContext and tests

---
+++ BuildingBlocks/MessageBus.Abstractions.Tests.Unit/MessageContextTests.cs
@@ -0,0 +1,37 @@
+﻿using MessageBus.Abstractions.Tests.Unit.Models.Events;
+using System;
+using Xunit;
+
+namespace MessageBus.Abstractions.Tests.Unit
+{
+    public class MessageContextTests
+    {
+        private const string _aircraftId = "3df0e37b-78cf-4ec5-973f-f56778e38be1";
+        private readonly string _messageAsString = $"{{\"AircraftId\": \"{_aircraftId}\"}}";
+        
+        [Fact]
+        public void ReturnsMessageAsString()
+        {
+            var sut = new MessageContext<AircraftLanded>
+            {
+                Body = new BinaryData(_messageAsString)
+            };
+
+            Assert.Equal(_messageAsString, sut.Body.ToString());
+        }
+        
+        [Fact]
+        public void ReturnsDeserializedMessage()
+        {
+            var sut = new MessageContext<AircraftLanded>
+            {
+                Body = new BinaryData(_messageAsString)
+            };
+
+            var result = sut.Body.ToObjectFromJson<AircraftLanded>();
+
+            Assert.Equal(_aircraftId, result.AircraftId);
+            Assert.Equal(_aircraftId, sut.Message.AircraftId);
+        }
+    }
+}
---
+++ BuildingBlocks/MessageBus.Abstractions/MessageContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Text.Json;
+
+namespace MessageBus.Abstractions
+{
+    public class MessageContext<T> where T : IMessage
+    {
+        public BinaryData Body { get; internal set; }
+        public T Message => JsonSerializer.Deserialize<T>(Body.ToString());
+    }
+}
