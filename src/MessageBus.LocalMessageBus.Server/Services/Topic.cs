5f6fb5adb59319abec118232a1d7f59af0f8cc0e
Mark Gossa <16326726+markgossa@users.noreply.github.com>
2021-03-03 23:58:11 +0000

    Adds get and update subscription

--- src/MessageBus.LocalMessageBus.Server/Services/Topic.cs
+++ src/MessageBus.LocalMessageBus.Server/Services/Topic.cs
@@ -1,6 +1,8 @@
 ﻿using MessageBus.LocalMessageBus.Server.MessageEntities;
 using MessageBus.LocalMessageBus.Server.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MessageBus.LocalMessageBus.Server.Services
 {
@@ -9,7 +11,13 @@ namespace MessageBus.LocalMessageBus.Server.Services
         private readonly List<Subscription> _subscriptions = new List<Subscription>();
 
         public void AddSubscription(Subscription subscription) => _subscriptions.Add(subscription);
-        public void RemoveSubscription(Subscription subscription) => _subscriptions.Remove(subscription);
+        public void RemoveSubscription(string name)
+        {
+            var subscription = _subscriptions.First(s => s.Name == name)
+                ?? ThrowIfSubscriptionNotFound(name);
+
+            _subscriptions.Remove(subscription);
+        }
 
         public void Send(LocalMessage message)
         {
@@ -18,5 +26,19 @@ namespace MessageBus.LocalMessageBus.Server.Services
                 subscription.Send(message);
             }
         }
+
+        public List<Subscription> GetSubscriptions() => _subscriptions;
+
+        public void UpdateSubsription(Subscription subscription, string name)
+        {
+            var subscriptionToUpdate = _subscriptions.FirstOrDefault(s => s.Name == name)
+                ?? ThrowIfSubscriptionNotFound(name);
+
+            subscriptionToUpdate.Label = subscription.Label;
+            subscriptionToUpdate.MessageProperties = subscription.MessageProperties;
+        }
+
+        private static Subscription ThrowIfSubscriptionNotFound(string name)
+            => throw new InvalidOperationException($"Subscription {name} not found");
     }
 }
--- tests/MessageBus.LocalMessageBus.Server.Tests.Unit/TopicTests.cs
+++ tests/MessageBus.LocalMessageBus.Server.Tests.Unit/TopicTests.cs
@@ -3,6 +3,7 @@ using MessageBus.LocalMessageBus.Server.Models;
 using MessageBus.LocalMessageBus.Server.Services;
 using Moq;
 using System;
+using System.Linq;
 using Xunit;
 
 namespace MessageBus.LocalMessageBus.Server.Tests.Unit
@@ -54,13 +55,58 @@ namespace MessageBus.LocalMessageBus.Server.Tests.Unit
             var sut = new Topic();
             sut.AddSubscription(subscription1);
             sut.AddSubscription(subscription2);
-            sut.RemoveSubscription(subscription2);
+            sut.RemoveSubscription(subscription2.Name);
             var messageBody = Guid.NewGuid().ToString();
             var message = new LocalMessage(messageBody);
             sut.Send(message);
 
             mockQueue1.Verify(m => m.Send(message), Times.Once);
             mockQueue2.Verify(m => m.Send(message), Times.Never); 
+        }
+
+        [Fact]
+        public void UpdatesExistingSubscription()
+        {
+            var mockQueue = new Mock<IQueue>();
+            var initialSubscription = new Subscription(mockQueue.Object, "Subscription1");
+            var newSubscription = new Subscription(mockQueue.Object, "Subscription1")
+            {
+                Label = "MyLabel",
+                MessageProperties = new()
+                {
+                    { "MessageType", "AircraftLanded"},
+                    { "AircraftType", "Commercial"}
+                }
+            };
+
+            var sut = new Topic();
+            sut.AddSubscription(initialSubscription);
+            sut.UpdateSubsription(newSubscription, "Subscription1");
+            var subscriptions = sut.GetSubscriptions();
+
+            Assert.Single(subscriptions);
+            Assert.Single(subscriptions.Where(s => s.Label == newSubscription.Label));
+            Assert.Single(subscriptions.Where(s => s.MessageProperties == newSubscription.MessageProperties));
+        }
+        
+        [Fact]
+        public void ThrowsIfRemoveUnknownSubscription()
+        {
+            var sut = new Topic();
+
+            Assert.Throws<InvalidOperationException>(() => sut.RemoveSubscription("InvalidSubscription"));
+        }
+        
+        [Fact]
+        public void ThrowsIfUpdateUnknownSubscription()
+        {
+            var sut = new Topic();
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var subscription = new Subscription(new Queue(), "InvalidSubscripton");
+                sut.UpdateSubsription(subscription, "InvalidSubscripton");
+            });
         }
     }
 }
